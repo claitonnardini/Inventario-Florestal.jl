acdace5e23b682f728732bb3b35a787cdf088e88
Claiton Nardini <68560327+claitonnardini@users.noreply.github.com>
2022-10-17 06:55:52 -0300

    Update 009 - Amostragem dupla.jl

--- Processos de amostragem/009 - Amostragem dupla.jl
+++ Processos de amostragem/009 - Amostragem dupla.jl
@@ -555,7 +555,7 @@ ________________________________________________________________________________
 
 #Processamento do inventário
 #Importar dados
-Dados = CSV.read("F:/Version_09_07_21/AD.csv", DataFrame) 
+Dados = CSV.read("F:/Version_09_07_21/ad.csv", DataFrame) 
 #Informações necessárias
 #Área da população
 const N = 500
