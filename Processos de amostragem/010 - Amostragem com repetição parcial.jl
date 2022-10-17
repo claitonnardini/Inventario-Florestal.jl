aa0c8f04b2dc5d6f88266b78656539d42cfd45eb
Claiton Nardini <68560327+claitonnardini@users.noreply.github.com>
2022-10-17 06:56:59 -0300

    Update 010 - Amostragem com repetição parcial.jl

--- Processos de amostragem/010 - Amostragem com repetição parcial.jl
+++ Processos de amostragem/010 - Amostragem com repetição parcial.jl
@@ -618,7 +618,7 @@ ________________________________________________________________________________
 
 #Processamento do inventário
 #Importar dados
-Dados = CSV.read("F:/Version_09_07_21/ARP.csv", DataFrame) 
+Dados = CSV.read("F:/Version_09_07_21/arp.csv", DataFrame) 
 #Informações necessárias
 #Área da população
 const N = 1582
